3515b554abedf802fbedabb7f28a08086b8ca1d9
BobRazowski <bobongithub@gmail.com>
2015-07-19 20:14:08 +0200

    add useful tool link

--- spec/features/mac_spec.rb
+++ spec/features/mac_spec.rb
@@ -10,6 +10,13 @@ feature 'Choose Essential || Useful tools' do
     expect(current_path).to eq ('/mac/essential-tools')
   end
 
+  scenario 'A user can select the useful tool' do
+    visit '/mac/'
+    expect(page).to have_content('Useful tools')
+    click_link 'Useful tools'
+    expect(current_path).to eq ('/mac/useful-tools')
+  end
+
 end
 
 feature 'Esential tool page' do
