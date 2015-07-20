842e2956730486fe02f1e9052080fd09c00c6200
BobRazowski <bobongithub@gmail.com>
2015-07-20 16:30:31 +0200

    gulp set up, ready to be tested

--- frontend/gulpfile.js
+++ frontend/gulpfile.js
@@ -1,21 +1,62 @@
+'use strict';
 var gulp = require('gulp'),
     sass = require('gulp-ruby-sass'),
     autoprefixer = require('gulp-autoprefixer'),
     minifycss = require('gulp-minify-css'),
-    jshint = require('gulp-jshint'),
     uglify = require('gulp-uglify'),
     imagemin = require('gulp-imagemin'),
     rename = require('gulp-rename'),
     concat = require('gulp-concat'),
     cache = require('gulp-cache'),
     livereload = require('gulp-livereload'),
-    del = require('del');
+    del = require('del'),
+    sass = require('gulp-sass'),
+    clean = require('gulp-clean'),
+    jsmin  = require("gulp-jsmin");
 
 gulp.task('default', function() {
-  return sass('../app/assets/stylesheets/application.scss', { style: 'expanded' })
+    gulp.start('styles', 'scripts', 'images', 'watch');
+});
+
+// gulp.task('clean', function(cb) {
+//     del(['../public/assets/stylesheets', '../public/assets/js', '../public/assets/images'], cb)
+// });
+
+gulp.task('images', function() {
+  return gulp.src('assets/images/**/*')
+    .pipe(cache(imagemin({ optimizationLevel: 5, progressive: true, interlaced: true })))
+    .pipe(gulp.dest('../public/assets/images'))
+});
+
+gulp.task('styles', function() {
+  return gulp.src('assets/stylesheets/*.scss', { style: 'expanded' })
+    .pipe(sass.sync().on('error', sass.logError))
     .pipe(autoprefixer('last 2 version'))
-    .pipe(gulp.dest('../public/assets/stylesheets'))
-    .pipe(rename({suffix: '.min'}))
+    .pipe(concat('main.css'))
     .pipe(minifycss())
+    .pipe(rename({suffix: '.min'}))
     .pipe(gulp.dest('../public/assets/stylesheets'))
+});
+
+gulp.task('scripts', function() {
+  return gulp.src('assets/javascripts/*.js')
+    .pipe(concat('main.js'))
+    .pipe(jsmin())
+    .pipe(uglify())
+    .pipe(rename({suffix: '.min'}))
+    .pipe(gulp.dest('../public/assets/js'))
+});
+
+
+gulp.task('watch', function() {
+
+  // Watch .scss files
+  gulp.watch('assets/stylesheets/**/*.scss', ['styles']);
+
+  // Watch .js files
+  gulp.watch('assets/javascripts/**/*.js', ['scripts']);
+
+  // Watch image files
+  gulp.watch('assets/images/**/*', ['images']);
+
 });
