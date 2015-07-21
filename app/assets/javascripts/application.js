12ec96c93f0f3fca638f1c7b5aad9d8c01bc64d9
BobRazowski <bobongithub@gmail.com>
2015-07-21 13:54:14 +0200

    fiw asset pipe

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,7 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery
+//= require jquery2
 //= require jquery_ujs
 //= require bootstrap-sprockets
 //= require turbolinks
@@ -20,16 +20,38 @@
 
 new WOW().init();
 
-$(document).pjax('a', '#pjax_container');
+    $(document).pjax('a', '#pjax_container');
+
+
+    // var duration = 400
+    // $('#pjax_container').pjax('a', { duration: duration })
+    // $('#pjax_container').on('pjax:start', function() { $(this).fadeOut(duration) }).on('pjax:waiting', function() {
+    //   $(this).show();
+    //   console.log("ca marche")
+    // }).on('pjax:end',   function() { $(this).fadeIn(duration) })
+
+
+        $('a').bind('click',function(e) {
+
+           e.stopPropagation();
+
+            $(this).parent().slideUp( 800 , 'ease', function(){
+              console.log('before click');
+                $(this).unbind('click').click();
+              console.log('after click');
+            });
+
+            console.log('it worked');
+            
+        });
+
+
+
 
 $(document).on("pjax:success", function(e) {
-  console.log("init wow");
   new WOW().init();
-  console.log("prepping the hover");
-
   $('.essentialtools__col').hover(
     function() {
-      console.log("doing the thing");
       $(this).removeClass('essentialtools__col--other').addClass('essentialtools__col--hover');
       $('.essentialtools__col').not($(this)).addClass('essentialtools__col--other');
     }, function() {
@@ -37,3 +59,12 @@ $(document).on("pjax:success", function(e) {
       $('.essentialtools__col').not($(this)).removeClass('essentialtools__col--other');
    });
 });
+
+$('.essentialtools__col').hover(
+  function() {
+    $(this).removeClass('essentialtools__col--other').addClass('essentialtools__col--hover');
+    $('.essentialtools__col').not($(this)).addClass('essentialtools__col--other');
+  }, function() {
+    $(this).removeClass('essentialtools__col--other, essentialtools__col--hover');
+    $('.essentialtools__col').not($(this)).removeClass('essentialtools__col--other');
+ });
