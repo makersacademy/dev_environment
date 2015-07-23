acb83b42542fd9da1a2e4be515b3ec885b27fa94
BobRazowski <bobongithub@gmail.com>
2015-07-23 14:27:06 +0200

    fix fittext a

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -58,14 +58,16 @@ $(document).on("pjax:success", function(e) {
   jQuery('.carousel').on('slid.bs.carousel', function () {
     $('h2').fitText(1.2, { minFontSize: '32px', maxFontSize: '240px' });
     $('p').fitText(1.2, { minFontSize: '14px', maxFontSize: '26px' });
-    $('p.step__next').fitText(1, { minFontSize: '12px', maxFontSize: '32px' });
+    $('p.step__next').fitText(1, { minFontSize: '12px', maxFontSize: '62px' });
+    $('a.step__next').fitText(1, { minFontSize: '12px', maxFontSize: '62px' });
   });
 
 });
 
 $('h2').fitText(1.2, { minFontSize: '32px', maxFontSize: '240px' });
 $('p').fitText(1.2, { minFontSize: '14px', maxFontSize: '26px' });
-$('p.step__next').fitText(1, { minFontSize: '12px', maxFontSize: '32px' });
+$('p.step__next').fitText(1, { minFontSize: '12px', maxFontSize: '62px' });
+$('a.step__next').fitText(1, { minFontSize: '12px', maxFontSize: '62px' });
 
 //Allow to have the hover animation on the 5 columns grid
 $('.essentialtools__col').hover(
--- app/assets/javascripts/install.js
+++ app/assets/javascripts/install.js
@@ -3,7 +3,8 @@
 jQuery('.carousel').on('slid.bs.carousel', function () {
   $('h2').fitText(1.2, { minFontSize: '32px', maxFontSize: '240px' });
   $('p').fitText(1.2, { minFontSize: '14px', maxFontSize: '26px' });
-  $('p.step__next').fitText(1, { minFontSize: '12px', maxFontSize: '32px' });
+  $('p.step__next').fitText(1, { minFontSize: '12px', maxFontSize: '62px' });
+  $('a.step__next').fitText(1, { minFontSize: '12px', maxFontSize: '62px' });
 });
 
 var bgColor = $(".carousel__1").css('background-color');
